62f33aaeb317ec38fbb398f0feddc63758708765
Reinholdsson <nbte@rocketmail.com>
2020-11-01 13:09:00 +0100

    minor change to landing

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -2,31 +2,33 @@ import React, { useState } from 'react';
 import Axios from 'axios';
 
 export const Landing = () => {
-  const [data, setData] = useState('');
+  const [data, setData] = useState();
   const [search, setSearch] = useState('');
 
   const fetchDataFromExternalAPI = () => {
-    Axios.get(`https://pokeapi.co/api/v2/pokemon/${search}`)
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${search.toLowerCase()}`)
       .then((response) => setData(response.data))
-      .catch((error) => setSearch('Could not find your Pokemon'));
+      .catch((error) => console.log(error));
   };
 
   const displayPokemonData = () => {
-    return (
-      <div>
-        <h1>name: {data.name}</h1>
-        <h1>id: {data.id}</h1>
-        <h1>height: {data.height}</h1>
-        <h1>weight: {data.weight}</h1>
-      </div>
-    );
+    if (data) {
+      return (
+        <div>
+          <h1>name: {data.name}</h1>
+          <h1>id: {data.id}</h1>
+          <h1>height: {data.height}</h1>
+          <h1>weight: {data.weight}</h1>
+          <h1>type: {data.types[0].type.name}</h1>
+        </div>
+      );
+    }
   };
 
   return (
     <div>
       <input onChange={(e) => setSearch(e.target.value)} />
       <button onClick={() => fetchDataFromExternalAPI()}>Search</button>
-      <h1>{search}</h1>
       {displayPokemonData()}
     </div>
   );
