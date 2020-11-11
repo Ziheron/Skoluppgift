34ed2a121b462324a569c1a4f6215536d04dc80b
Reinholdsson <nbte@rocketmail.com>
2020-11-11 16:41:31 +0100

    Skolprojekt

---
+++ server/Server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+import middlewares from './src/middlewares/Middlewares.js';
+import configuration from './configuration/Configuration.js';
+import UserRoutes from './src/routes/User.routes.js';
+
+dotenv.config();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(morgan('common'));
+// eslint-disable-next-line no-undef
+
+UserRoutes.routes(app);
+
+app.get('/moose', (req, res) => {
+  res.send('Man skall inte välta den älg som sover!');
+});
+
+UserRoutes.routes(app);
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+configuration.connectToDatabase;
+configuration.connectToPort(app);
---
+++ server/configuration/Configuration.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectToDatabase = async () => {
+  try {
+    const DB_URL = process.env.DATABASE_URL;
+    await mongoose.connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Successfully connected to the database!');
+  } catch (error) {
+    console.log('ERROR WHILE TRYING TO CONNECT TO THE DATABASE: ', error);
+    process.exit();
+  }
+};
+
+const connectToPort = (app) => {
+  const port = process.env.PORT;
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+  });
+};
+
+export default {
+  connectToDatabase,
+  connectToPort,
+};
